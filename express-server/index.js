6c6fc5d769152852630cced4f00e2dcf7baf017a
DebjitPramanick <dpramanick9990@gmail.com>
2021-06-11 07:43:10 +0530

    Connecting mongoose

--- express-server/index.js
+++ express-server/index.js
@@ -2,8 +2,21 @@ const express = require('express')
 const cors = require('cors');
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema/Schema.js')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express()
+
+const URL = process.env.CONNECTION_URL
+mongoose.connect(URL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.once('open', () => {
+    console.log("Connected to database...")
+})
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
--- express-server/schema/Schema.js
+++ express-server/schema/Schema.js
@@ -1,26 +1,56 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+const Book = require("../models/Book.js")
+const Author = require("../models/Author.js")
 
 // GraphQL Schema
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql
 
 
 var books = [
-    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '2' },
-    { name: 'Haunted House', genre: 'Horror', id: '3' },
-    { name: 'Galactic', genre: 'Sci-Fi', id: '4' },
-    { name: 'Maze Runner', genre: 'Adventure', id: '5' },
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '2', authorId: '2' },
+    { name: 'Haunted House', genre: 'Horror', id: '3', authorId: '1' },
+    { name: 'Galactic', genre: 'Sci-Fi', id: '4', authorId: '2' },
+    { name: 'Maze Runner', genre: 'Adventure', id: '5', authorId: '3' },
+]
+
+var authors = [
+    { name: 'Patrick Rothfuss', age: 44, id: '1' },
+    { name: 'David Johanson', age: 56, id: '2' },
+    { name: 'Mark Sanderson', age: 39, id: '3' },
 ]
     
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type : GraphQLString },
+        id: { type : GraphQLID },
         name: { type : GraphQLString },
-        genre: { type : GraphQLString }
+        genre: { type : GraphQLString },
+        author: { 
+            type : AuthorType,
+            resolve(parent, args){
+                // return _.find(authors, {id: parent.authorId})
+            }
+        }
+    })
+})
+
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type : GraphQLID },
+        name: { type : GraphQLString },
+        age: { type : GraphQLInt },
+        books: {
+            type : new GraphQLList(BookType),
+            resolve(parent, args){
+                // return _.filter(books, {authorId: parent.id})
+            }
+        }
     })
 })
 
@@ -30,13 +60,36 @@ const RootQueryType = new GraphQLObjectType({
     fields: {
         book: { 
             type : BookType,
-            args : { id : { type : GraphQLString } },
+            args : { id : { type : GraphQLID } },
             resolve(parent, args){
                 // Code to get data from db/other source
                 
-                return _.find(books, {id: args.id});
+                // return _.find(books, {id: args.id});
             }
-        }
+        },
+        author: { 
+            type : AuthorType,
+            args : { id : { type : GraphQLID } },
+            resolve(parent, args){
+                // Code to get data from db/other source
+                
+                // return _.find(authors, {id: args.id});
+            }
+        },
+
+        // Query for list of books, authors
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                // return books
+            }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args){
+                // return authors
+            }
+        },
     }
 })
 
