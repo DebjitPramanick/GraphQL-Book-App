812fe24e682a1a9c598f0014cd437fdd3885b761
DebjitPramanick <dpramanick9990@gmail.com>
2021-06-12 19:44:30 +0530

    Non null

--- express-server/schema/Schema.js
+++ express-server/schema/Schema.js
@@ -5,7 +5,15 @@ const Author = require("../models/Author.js")
 
 // GraphQL Schema
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql
+const { 
+    GraphQLObjectType, 
+    GraphQLString, 
+    GraphQLSchema, 
+    GraphQLID, 
+    GraphQLInt, 
+    GraphQLList, 
+    GraphQLNonNull 
+} = graphql
 
 
 var books = [
@@ -100,8 +108,9 @@ const Mutation = new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args: {
-                name: {type: GraphQLString},
-                age: {type: GraphQLInt}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLInt)}
+                
             },
             async resolve(parent, args){
                 let author = new Author({
@@ -116,9 +125,9 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                name: {type: GraphQLString},
-                genre: {type: GraphQLString},
-                authorId: {type: GraphQLID}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                genre: {type: new GraphQLNonNull(GraphQLString)},
+                authorId: {type: new GraphQLNonNull(GraphQLID)}
             },
             async resolve(parent, args){
                 let book = new Book({
